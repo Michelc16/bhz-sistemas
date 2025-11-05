33d5630903a9a2535c9a08cefdc7143d297c965a
Michelc16 <michel.dev.atual@gmail.com>
2025-11-05 13:01:29 -0300

    modulo company_type_extend

---
+++ addons/bhz_company_type_extend/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "BHZ Company Type Extend",
+    "summary": "Campos extras em empresas/contatos (tipos, fiscais, etc.).",
+    "version": "19.0.1.0.0",
+    "author": "BHZ Sistemas",
+    "website": "https://bhzsistemas.com.br",
+    "category": "Contacts",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "data": [],
+    "installable": True,
+    "application": True,
+}
