074198eb0d67c47d445ed0b347e31fb7ff9e90fa
Michelc16 <michel.dev.atual@gmail.com>
2025-11-05 15:47:07 -0300

    correção branding_safe add sale multi pay

---
+++ addons/bhz_sale_multi_payment/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "BHZ Sale Multi Payment",
+    "summary": "Múltiplas formas de pagamento na venda",
+    "version": "19.0.1.0.0",
+    "author": "BHZ Sistemas",
+    "website": "https://bhzsistemas.com.br",
+    "category": "Sales",
+    "license": "LGPL-3",
+    "depends": ["sale_management", "account"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
