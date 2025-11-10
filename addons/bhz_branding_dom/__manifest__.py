ac4a61a137b945489f31d730952bcf718959609f
Michelc16 <michel.dev.atual@gmail.com>
2025-11-10 12:43:34 -0300

    ajuste branding

--- addons/bhz_branding_dom/__manifest__.py
+++ addons/bhz_branding_dom/__manifest__.py
@@ -1,23 +1,16 @@
 {
     "name": "BHZ Branding (DOM)",
-    "summary": "Aplica marca e cores da BHZ SISTEMAS sem herdar views de branding da Odoo.",
     "version": "19.0.1.0.0",
+    "summary": "Branding BHZ SISTEMAS - title, favicon, logo and UI tweaks",
+    "category": "Tools",
     "author": "BHZ SISTEMAS",
     "website": "https://www.bhzsistemas.com.br",
     "license": "LGPL-3",
-    "category": "Tools",
     "depends": ["web", "website"],
-    "data": [],
-    "assets": {
-        "web.assets_backend": [
-            "bhz_branding_dom/static/src/js/bhz_backend.js",
-            "bhz_branding_dom/static/src/scss/bhz_theme.scss",
-        ],
-        "web.assets_frontend": [
-            "bhz_branding_dom/static/src/js/bhz_website.js",
-            "bhz_branding_dom/static/src/scss/bhz_theme.scss",
-        ],
-    },
+    "data": [
+        "views/assets.xml",
+    ],
     "installable": True,
     "application": False,
+    "auto_install": False,
 }
