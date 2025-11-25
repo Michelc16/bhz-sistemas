994d0d89c8a8cf042e0127e782f8c46438be9769
Michelc16 <michel.dev.atual@gmail.com>
2025-11-25 14:28:15 -0300

    ajuste magalu

--- addons/bhz_magalu_connector/hooks.py
+++ addons/bhz_magalu_connector/hooks.py
@@ -1,11 +1,8 @@
-from odoo import SUPERUSER_ID, api
-
 CLIENT_ID = "dZCVzEyLat_rtRfHvNAuulhBUZBlz_6Lj_NZghlU7Qw"
 CLIENT_SECRET = "wBI9y2hW604gkBpfxrTWoraU2UtVBC-BHWgVMn-XQE0"
 
 
-def pre_init_set_magalu_client(cr):
-    env = api.Environment(cr, SUPERUSER_ID, {})
+def pre_init_set_magalu_client(env):
     params = env["ir.config_parameter"].sudo()
     if not params.get_param("bhz_magalu.client_id"):
         params.set_param("bhz_magalu.client_id", CLIENT_ID, groups="base.group_system")
