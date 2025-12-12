2a46f34575fc386578a1ebd59e885c6c9c667baf
Michelc16 <michel.dev.atual@gmail.com>
2025-12-12 15:52:03 -0300

    ajuste dre

--- addons/bhz_dre_cmv/__manifest__.py
+++ addons/bhz_dre_cmv/__manifest__.py
@@ -1,21 +1,27 @@
 {
-    'name': 'BHZ DRE / CMV',
-    'version': '17.0.1.0.0',
-    'summary': 'Demonstração de Resultado do Exercício (DRE) com cálculo de CMV',
-    'category': 'Accounting/Reporting',
-    'author': 'BHZ Sistemas',
-    'license': 'OPL-1',
-    'depends': ['account'],
-    'data': [
-        'security/security.xml',
-        'security/ir.model.access.csv',
-        'views/bhz_dre_menu.xml',
-        'views/bhz_dre_template_views.xml',
-        'views/bhz_dre_wizard_views.xml',
-        'views/bhz_dre_report_views.xml',
-        'report/bhz_dre_report_templates.xml',
-        'report/bhz_dre_report_action.xml',
+    "name": "BHZ DRE + CMV",
+    "version": "19.0.1.0.0",
+    "category": "Accounting",
+    "summary": "DRE com template configurável e geração mensal",
+    "depends": ["account"],
+    "data": [
+        # segurança primeiro
+        "security/security.xml",
+        "security/ir.model.access.csv",
+
+        # views (actions dentro do xml do wizard ou em action separado)
+        "views/bhz_dre_template_views.xml",
+        "views/bhz_dre_report_views.xml",
+        "views/bhz_dre_wizard_views.xml",
+
+        # report qweb + action do report
+        "report/bhz_dre_report_templates.xml",
+        "report/bhz_dre_report_action.xml",
+
+        # menu por último (depende de actions já existirem)
+        "views/bhz_dre_menu.xml",
     ],
-    'installable': True,
-    'application': False,
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
 }
