045113b619a62ff206ecd45fba0107db9908a7d7
Michelc16 <michel.dev.atual@gmail.com>
2025-12-18 11:38:12 -0300

    ajuste omni

---
+++ addons/guiabh_blog_hide_meta/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "GUIA BH - Hide Blog Date/Author",
+    "version": "1.0.0",
+    "category": "Website",
+    "summary": "Esconde data/autor/meta nas páginas de post do blog (website_blog).",
+    "depends": ["website", "website_blog"],
+    "data": [
+        "views/assets.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
