602800913bee958ac23ed9bc67eb3d2a26380347
Michelc16 <michel.dev.atual@gmail.com>
2025-12-30 09:50:08 -0300

    ajuste

--- addons/bhz_event_promo/__manifest__.py
+++ addons/bhz_event_promo/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": "Agenda de eventos com suporte a eventos de terceiros, link externo e botão personalizável.",
     "author": "BHZ Sistemas",
     "license": "LGPL-3",
-    "depends": ["base", "website", "web_editor", "event", "website_event", "event_sale"],
+    "depends": ["base", "website", "event", "website_event", "event_sale"],
     "data": [
         "security/security.xml",
         "security/ir.model.access.csv",
