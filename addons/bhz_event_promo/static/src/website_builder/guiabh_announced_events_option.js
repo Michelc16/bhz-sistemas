0012681b89ab6fb99e5971d4feb6de25c8f5e938
Michelc16 <michel.dev.atual@gmail.com>
2026-01-05 15:39:22 -0300

    ajuste

--- addons/bhz_event_promo/static/src/website_builder/guiabh_announced_events_option.js
+++ addons/bhz_event_promo/static/src/website_builder/guiabh_announced_events_option.js
@@ -1,49 +1,33 @@
 /** @odoo-module **/
 
-import {
-    DYNAMIC_SNIPPET,
-    setDatasetIfUndefined,
-} from "@website/builder/plugins/options/dynamic_snippet_option_plugin";
+import { SNIPPET_SPECIFIC } from "@html_builder/utils/option_sequence";
 import { BaseOptionComponent } from "@html_builder/core/utils";
-import { useDynamicSnippetOption } from "@website/builder/plugins/options/dynamic_snippet_hook";
 import { Plugin } from "@html_editor/plugin";
 import { withSequence } from "@html_editor/utils/resource";
 import { registry } from "@web/core/registry";
 
 export class GuiabhAnnouncedEventsOption extends BaseOptionComponent {
     static template = "bhz_event_promo.GuiabhAnnouncedEventsOption";
-    static dependencies = ["dynamicSnippetOption"];
     static selector = ".s_guiabh_announced_events";
-
-    setup() {
-        super.setup();
-        const { getModelNameFilter } = this.dependencies.dynamicSnippetOption;
-        this.dynamicOptionParams = useDynamicSnippetOption(getModelNameFilter());
-    }
 }
 
 class GuiabhAnnouncedEventsOptionPlugin extends Plugin {
     static id = "guiabhAnnouncedEventsOption";
-    static dependencies = ["dynamicSnippetOption"];
     resources = {
-        builder_options: withSequence(DYNAMIC_SNIPPET, GuiabhAnnouncedEventsOption),
-        on_snippet_dropped_handlers: this.onSnippetDropped.bind(this),
+        builder_options: [withSequence(SNIPPET_SPECIFIC, GuiabhAnnouncedEventsOption)],
+        on_snippet_dropped_handlers: (params) => this.onSnippetDropped(params),
     };
 
-    getModelNameFilter() {
-        return "event.event";
-    }
-
-    async onSnippetDropped({ snippetEl }) {
+    onSnippetDropped({ snippetEl }) {
         if (!snippetEl.matches(GuiabhAnnouncedEventsOption.selector)) {
             return;
         }
-        setDatasetIfUndefined(snippetEl, "limit", "12");
-        setDatasetIfUndefined(snippetEl, "categoryIds", "[]");
-        await this.dependencies.dynamicSnippetOption.setOptionsDefaultValues(
-            snippetEl,
-            this.getModelNameFilter()
-        );
+        if (!snippetEl.dataset.limit) {
+            snippetEl.dataset.limit = "12";
+        }
+        if (!snippetEl.dataset.categoryIds) {
+            snippetEl.dataset.categoryIds = "[]";
+        }
     }
 }
 
