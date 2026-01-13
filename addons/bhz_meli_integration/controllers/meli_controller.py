f925359147d6de6f9e6d9c29ce86bed3e1cd9fea
Michelc16 <michel.dev.atual@gmail.com>
2026-01-13 17:33:22 -0300

    ajuste

--- addons/bhz_meli_integration/controllers/meli_controller.py
+++ addons/bhz_meli_integration/controllers/meli_controller.py
@@ -14,24 +14,31 @@ class MeliAuthController(http.Controller):
         Ela recebe ?code=... e opcionalmente ?state=<id_da_conta>
         """
         code = kwargs.get("code")
-        account_id = kwargs.get("state")
+        state = kwargs.get("state")
+        _logger.info("[ML][OAuth] Callback recebido. code? %s | state=%s", bool(code), state)
 
-        if not code:
-            return "Faltou o code do Mercado Livre."
-
-        if account_id:
-            account = request.env["meli.account"].sudo().browse(int(account_id))
-        else:
-            # pega a primeira conta em rascunho
-            account = request.env["meli.account"].sudo().search([("state", "=", "draft")], limit=1)
-
-        if not account:
-            return "Nenhuma conta do Mercado Livre encontrada para vincular."
+        if not code or not state:
+            return "Faltou o code/state do Mercado Livre. Gere a autorização novamente."
 
         try:
-            account.sudo().exchange_code_for_token(code)
+            account_id = int(state.split(":", 1)[0])
+        except Exception:
+            _logger.warning("[ML][OAuth] State inválido recebido: %s", state)
+            return "State inválido recebido. Gere a autorização novamente."
+
+        account = request.env["meli.account"].sudo().browse(account_id)
+        if not account or not account.exists():
+            return "Conta do Mercado Livre não encontrada."
+
+        try:
+            account = account.sudo().with_company(account.company_id)
+            account.exchange_code_for_token(code, state=state)
         except Exception as e:
-            _logger.exception("Erro ao autenticar conta Mercado Livre")
+            _logger.exception("[ML][OAuth] Erro ao autenticar conta Mercado Livre %s", account.display_name)
+            try:
+                account._record_error(str(e))
+            except Exception:
+                _logger.debug("Não foi possível registrar erro na conta ML", exc_info=True)
             return "Erro ao autenticar: %s" % str(e)
 
         return "Conta Mercado Livre conectada com sucesso. Você já pode fechar esta aba."
