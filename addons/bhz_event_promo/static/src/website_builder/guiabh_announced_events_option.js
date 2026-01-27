98d97a9e386e5cf93db9ac6b8051091c3d17e378
Michelc16 <michel.dev.atual@gmail.com>
2026-01-27 12:52:27 -0300

    ajuste

--- addons/bhz_event_promo/static/src/website_builder/guiabh_announced_events_option.js
+++ addons/bhz_event_promo/static/src/website_builder/guiabh_announced_events_option.js
@@ -1,78 +1,71 @@
 /** @odoo-module **/
-odoo.define("bhz_event_promo.guiabh_announced_events_option", [], function (require) {
-    "use strict";
 
-    let BaseOptionComponent, Plugin, SNIPPET_SPECIFIC, withSequence, registry;
-    try {
-        BaseOptionComponent = require("@html_builder/core/utils").BaseOptionComponent;
-        Plugin = require("@html_editor/plugin").Plugin;
-        SNIPPET_SPECIFIC = require("@html_builder/utils/option_sequence").SNIPPET_SPECIFIC;
-        withSequence = require("@html_editor/utils/resource").withSequence;
-        registry = require("@web/core/registry");
-    } catch (err) {
-        // If builder libs aren't available (public/minimal assets), skip safely.
-        return;
+import { registry } from "@web/core/registry";
+import { BaseOptionComponent } from "@html_builder/core/utils";
+import { Plugin } from "@html_editor/plugin";
+import { SNIPPET_SPECIFIC } from "@html_builder/utils/option_sequence";
+import { withSequence } from "@html_editor/utils/resource";
+
+class GuiabhAnnouncedEventsOption extends BaseOptionComponent {
+    static selector = ".s_guiabh_announced_events";
+    static template = "bhz_event_promo.GuiabhAnnouncedEventsOption";
+
+    setup() {
+        super.setup(...arguments);
+        this.limit = this._readLimit();
     }
 
-    class GuiabhAnnouncedEventsOption extends BaseOptionComponent {
-        static selector = ".s_guiabh_announced_events";
-        static template = "bhz_event_promo.GuiabhAnnouncedEventsOption";
-
-        setup() {
-            super.setup(...arguments);
-            this.limit = this._readLimit();
+    _readLimit() {
+        const section = this.el || document.createElement("div");
+        const raw = section.dataset?.limit || "12";
+        const parsed = parseInt(raw, 10);
+        if (Number.isNaN(parsed)) {
+            return 12;
         }
-
-        _readLimit() {
-            const section = this.el || document.createElement("div");
-            const raw = section.dataset?.limit || "12";
-            const parsed = parseInt(raw, 10);
-            if (Number.isNaN(parsed)) {
-                return 12;
-            }
-            return Math.min(Math.max(parsed, 1), 24);
-        }
-
-        onLimitInput(ev) {
-            const value = this._sanitize(ev.target.value);
-            ev.target.value = value;
-        }
-
-        onLimitChange(ev) {
-            const value = this._sanitize(ev.target.value);
-            ev.target.value = value;
-            this._applyLimit(value);
-        }
-
-        _sanitize(value) {
-            const parsed = parseInt(value || "0", 10) || 0;
-            return Math.min(Math.max(parsed, 1), 24);
-        }
-
-        _applyLimit(value) {
-            const section = this.el;
-            if (!section) {
-                return;
-            }
-            const valStr = String(value);
-            section.dataset.limit = valStr;
-            section.dispatchEvent(
-                new CustomEvent("change", {
-                    bubbles: true,
-                })
-            );
-            this.requestSave();
-        }
+        return Math.min(Math.max(parsed, 1), 24);
     }
 
-    class GuiabhAnnouncedEventsOptionPlugin extends Plugin {
-        static id = "guiabhAnnouncedEventsOption";
-        resources = {
-            builder_options: [withSequence(SNIPPET_SPECIFIC, GuiabhAnnouncedEventsOption)],
-        };
+    onLimitInput(ev) {
+        const value = this._sanitize(ev.target.value);
+        ev.target.value = value;
     }
 
+    onLimitChange(ev) {
+        const value = this._sanitize(ev.target.value);
+        ev.target.value = value;
+        this._applyLimit(value);
+    }
+
+    _sanitize(value) {
+        const parsed = parseInt(value || "0", 10) || 0;
+        return Math.min(Math.max(parsed, 1), 24);
+    }
+
+    _applyLimit(value) {
+        const section = this.el;
+        if (!section) {
+            return;
+        }
+        const valStr = String(value);
+        section.dataset.limit = valStr;
+        section.dispatchEvent(
+            new CustomEvent("change", {
+                bubbles: true,
+            })
+        );
+        this.requestSave();
+    }
+}
+
+class GuiabhAnnouncedEventsOptionPlugin extends Plugin {
+    static id = "guiabhAnnouncedEventsOption";
+    resources = {
+        builder_options: [withSequence(SNIPPET_SPECIFIC, GuiabhAnnouncedEventsOption)],
+    };
+}
+
+if (registry?.category) {
     registry
         .category("website-plugins")
         .add(GuiabhAnnouncedEventsOptionPlugin.id, GuiabhAnnouncedEventsOptionPlugin);
-});
+}
--- addons/bhz_event_promo/static/src/website_builder/guiabh_featured_carousel_option.js
+++ addons/bhz_event_promo/static/src/website_builder/guiabh_featured_carousel_option.js
@@ -1,85 +1,78 @@
 /** @odoo-module **/
-odoo.define("bhz_event_promo.guiabh_featured_carousel_option", [], function (require) {
-    "use strict";
 
-    let BaseOptionComponent, Plugin, SNIPPET_SPECIFIC, withSequence, registry;
-    try {
-        BaseOptionComponent = require("@html_builder/core/utils").BaseOptionComponent;
-        Plugin = require("@html_editor/plugin").Plugin;
-        SNIPPET_SPECIFIC = require("@html_builder/utils/option_sequence").SNIPPET_SPECIFIC;
-        withSequence = require("@html_editor/utils/resource").withSequence;
-        registry = require("@web/core/registry");
-    } catch (err) {
-        // If builder libs aren't available (public/minimal assets), skip safely.
-        return;
+import { registry } from "@web/core/registry";
+import { BaseOptionComponent } from "@html_builder/core/utils";
+import { Plugin } from "@html_editor/plugin";
+import { SNIPPET_SPECIFIC } from "@html_builder/utils/option_sequence";
+import { withSequence } from "@html_editor/utils/resource";
+
+class GuiabhFeaturedCarouselOption extends BaseOptionComponent {
+    static selector = ".s_guiabh_featured_carousel";
+    static template = "bhz_event_promo.GuiabhFeaturedCarouselOption";
+
+    setup() {
+        super.setup(...arguments);
+        this.interval = this._readInterval();
     }
 
-    class GuiabhFeaturedCarouselOption extends BaseOptionComponent {
-        static selector = ".s_guiabh_featured_carousel";
-        static template = "bhz_event_promo.GuiabhFeaturedCarouselOption";
-
-        setup() {
-            super.setup(...arguments);
-            this.interval = this._readInterval();
-        }
-
-        _readInterval() {
-            const section = this.el || document.createElement("div");
-            const carousel = section.querySelector?.(".guiabh-featured-carousel");
-            const raw = carousel?.dataset.interval || section.dataset?.interval || "5000";
-            const parsed = parseInt(raw, 10);
-            return Number.isNaN(parsed) ? 5000 : parsed;
-        }
-
-        onIntervalInput(ev) {
-            const value = this._sanitize(ev.target.value);
-            ev.target.value = value;
-        }
-
-        onIntervalChange(ev) {
-            const value = this._sanitize(ev.target.value);
-            ev.target.value = value;
-            this._applyInterval(value);
-        }
-
-        _sanitize(value) {
-            const parsed = parseInt(value || "0", 10) || 0;
-            const clamped = Math.min(Math.max(parsed, 1000), 20000);
-            return clamped;
-        }
-
-        _applyInterval(value) {
-            const section = this.el;
-            if (!section) {
-                return;
-            }
-            const carousel = section.querySelector
-                ? section.querySelector(".guiabh-featured-carousel")
-                : null;
-            const valStr = String(value);
-            section.dataset.interval = valStr;
-            if (carousel) {
-                carousel.dataset.interval = valStr;
-                carousel.dataset.bsInterval = valStr;
-            }
-            carousel?.dispatchEvent(
-                new CustomEvent("guiabh-featured-interval-update", {
-                    bubbles: true,
-                    detail: { interval: value },
-                })
-            );
-            this.requestSave();
-        }
+    _readInterval() {
+        const section = this.el || document.createElement("div");
+        const carousel = section.querySelector?.(".guiabh-featured-carousel");
+        const raw = carousel?.dataset.interval || section.dataset?.interval || "5000";
+        const parsed = parseInt(raw, 10);
+        return Number.isNaN(parsed) ? 5000 : parsed;
     }
 
-    class GuiabhFeaturedCarouselOptionPlugin extends Plugin {
-        static id = "guiabhFeaturedCarouselOption";
-        resources = {
-            builder_options: [withSequence(SNIPPET_SPECIFIC, GuiabhFeaturedCarouselOption)],
-        };
+    onIntervalInput(ev) {
+        const value = this._sanitize(ev.target.value);
+        ev.target.value = value;
     }
 
+    onIntervalChange(ev) {
+        const value = this._sanitize(ev.target.value);
+        ev.target.value = value;
+        this._applyInterval(value);
+    }
+
+    _sanitize(value) {
+        const parsed = parseInt(value || "0", 10) || 0;
+        const clamped = Math.min(Math.max(parsed, 1000), 20000);
+        return clamped;
+    }
+
+    _applyInterval(value) {
+        const section = this.el;
+        if (!section) {
+            return;
+        }
+        const carousel = section.querySelector
+            ? section.querySelector(".guiabh-featured-carousel")
+            : null;
+        const valStr = String(value);
+        section.dataset.interval = valStr;
+        if (carousel) {
+            carousel.dataset.interval = valStr;
+            carousel.dataset.bsInterval = valStr;
+        }
+        carousel?.dispatchEvent(
+            new CustomEvent("guiabh-featured-interval-update", {
+                bubbles: true,
+                detail: { interval: value },
+            })
+        );
+        this.requestSave();
+    }
+}
+
+class GuiabhFeaturedCarouselOptionPlugin extends Plugin {
+    static id = "guiabhFeaturedCarouselOption";
+    resources = {
+        builder_options: [withSequence(SNIPPET_SPECIFIC, GuiabhFeaturedCarouselOption)],
+    };
+}
+
+if (registry?.category) {
     registry
         .category("website-plugins")
         .add(GuiabhFeaturedCarouselOptionPlugin.id, GuiabhFeaturedCarouselOptionPlugin);
-});
+}
