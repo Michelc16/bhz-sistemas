f760591055142942b055949be98717ae97f1c193
Michelc16 <michel.dev.atual@gmail.com>
2026-02-05 15:33:56 -0300

    ajuste

--- addons/bhz_football_agenda/__manifest__.py
+++ addons/bhz_football_agenda/__manifest__.py
@@ -1,5 +1,5 @@
 {'name': 'BHZ - Agenda Futebol (Cruzeiro, Atlético-MG, América-MG)',
- 'version': '1.0.5',
+ 'version': '1.0.6',
  'category': 'Website',
  'summary': 'Página no site com agenda de jogos dos times de BH (Cruzeiro, Atlético-MG e América-MG).',
  'author': 'BHZ Sistemas',
@@ -18,6 +18,6 @@
           'views_website/res_config_settings_website.xml'],
  'assets': {'web.assets_frontend': ['bhz_football_agenda/static/src/scss/football_snippet.scss',
                                     'bhz_football_agenda/static/src/js/guiabh_football_matches.js'],
-            'website.website_builder_assets': ['bhz_football_agenda/static/src/website_builder/**/*']},
+            'website.assets_wysiwyg': ['bhz_football_agenda/static/src/website_builder/**/*']},
  'application': True,
  'installable': True}
