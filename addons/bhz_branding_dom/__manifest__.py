b563312dea564abd9a90fecf4ab7a1ea3293d8e8
Michelc16 <michel.dev.atual@gmail.com>
2026-02-11 14:32:49 -0300

    ajuste

--- addons/bhz_branding_dom/__manifest__.py
+++ addons/bhz_branding_dom/__manifest__.py
@@ -1,19 +1,15 @@
 {
     "name": "BHZ Branding (DOM)",
-    "version": "19.0.1.0.0",
-    "summary": "Branding BHZ SISTEMAS - title, favicon, logo and UI tweaks",
+    "version": "19.0.1.0.1",
+    "summary": "Branding BHZ SISTEMAS - title, favicon, logo and UI tweaks (backend only)",
     "category": "Tools",
     "author": "BHZ SISTEMAS",
     "website": "https://www.bhzsistemas.com.br",
     "license": "LGPL-3",
-    "depends": ["web", "website"],    
+    "depends": ["web", "website"],
     "data": [],
     "assets": {
         "web.assets_backend": [
-            "bhz_branding_dom/static/src/scss/bhz_branding.scss",
-            "bhz_branding_dom/static/src/js/bhz_branding.js",
-        ],
-        "web.assets_frontend": [
             "bhz_branding_dom/static/src/scss/bhz_branding.scss",
             "bhz_branding_dom/static/src/js/bhz_branding.js",
         ],
