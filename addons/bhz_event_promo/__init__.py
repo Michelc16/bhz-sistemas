06ea57a9b22537f383342c163b1fa00cbfc97a29
Michelc16 <michel.dev.atual@gmail.com>
2026-02-19 12:38:21 -0300

    ajuste

--- addons/bhz_event_promo/__init__.py
+++ addons/bhz_event_promo/__init__.py
@@ -2,3 +2,6 @@ from . import models
 from . import controllers
 from . import wizard
 from . import hooks
+
+# Expose hooks at module level (required by Odoo when referenced in __manifest__.py)
+from .hooks import post_init_hook
